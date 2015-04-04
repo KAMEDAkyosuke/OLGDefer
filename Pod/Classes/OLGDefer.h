dfba890fd1b649b61130ae5864f18dff6c9d4b3d
KAMEDAkyosuke <kameda-kyosuke@kayac.com>
2015-04-04 12:29:49 +0900

    ヘッダにコメント追加

--- Pod/Classes/OLGDefer.h
+++ Pod/Classes/OLGDefer.h
@@ -8,17 +8,30 @@
 
 #import <Foundation/Foundation.h>
 
+/** OLGDefer インスタンスの破棄時に呼び出される処理の型 */
 typedef void(^OLGDeferTask)();
 
 @interface OLGDefer : NSObject
 
+/** OLGDefer インスタンスを作成し、破棄時に呼び出される処理を登録する
+ *
+ * `defer(...)` マクロを使用すると変数宣言までやってくれるので便利
+ *
+ * @param task インスタンスの破棄時に呼び出される処理
+ * @return OLGDefer インスタンス
+ */
 +(instancetype)deferWithTask:(OLGDeferTask)task;
 
 @end
 
 // see. https://gist.github.com/kenpusney/b7a317c16831d2bbc6de
 #define OLGGensymImpl2(cn, ln) OLGGensym##c##cn##l##ln
-#define OLGGensymImpl1(cn, ln) OLGGensymImpl2(cn,ln)
+#define OLGGensymImpl1(cn, ln) OLGGensymImpl2(cn, ln)
 #define OLGGensym() OLGGensymImpl1(__COUNTER__, __LINE__)
+
+/** ユーティリティマクロ
+ *
+ * @param t OLGDeferTask 型。インスタンスの破棄時に呼び出される処理
+ */
 
 #define defer(t) __attribute__((unused)) OLGDefer *OLGGensym() = [OLGDefer deferWithTask:(t)]
